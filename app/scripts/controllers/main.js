a5d0aeffc21f976b80429d26ac44c9546b912873
Navaneeth Palreddy <navaneethpalreddy@gmail.com>
2016-12-14 17:51:50 -0600

    pro

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name navApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the navApp
+ */
+angular.module('navApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
